3e5be6114ae83c78e93bcc90a4e9cfcafa5dad38
Louie1033 <louissantibanez22@gmail.com>
2023-02-15 09:55:45 -0800

    Completed Unit 1
    finished unit 1 started plane challenge

--- PlayerControlSantibanez/Assets/Scripts/PlayerControl.cs
+++ PlayerControlSantibanez/Assets/Scripts/PlayerControl.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 12.0f;
+    private float turnSpeed = 40.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +19,13 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         //this will move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //this controlls the steering of the car
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
